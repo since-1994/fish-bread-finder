f501ba0d23fadd50becf35823683e6fca9f89c40
gimminseog <gm27376@gmail.com>
2021-12-08 00:21:03 +0900

    add: 멀티 마커 임시로 표시

--- src/components/KakaoMap/index.tsx
+++ src/components/KakaoMap/index.tsx
@@ -2,7 +2,8 @@
 import React, { useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import Layer from '../Layer';
-
+import markerImageSrc from '../../static/images/defaultMarker-3.png';
+import { getDatabase, ref, child, get } from "firebase/database";
 
 declare global {
   interface Window {
@@ -19,14 +20,14 @@ interface KakaoMapPosition{
   lon: Number;
 }
 
-
 const KakaoMap: React.FC<kakaoMapProps> = function ({ mode }) {
   const map = useRef(undefined);
   // const map2 = useRef(undefined);
+  const [list , setList] = useState<any[]>([]);
   const [willAddedPosition] = useState<KakaoMapPosition>({lat: 0, lon: 0});
 
-  function getCurrentPosition() {
-    try{
+  async function getCurrentPosition(): Promise<KakaoMapPosition> {
+    return new Promise<KakaoMapPosition>((resolve) => {
       navigator.geolocation.getCurrentPosition(
         (position) => {
           const {
@@ -35,26 +36,73 @@ const KakaoMap: React.FC<kakaoMapProps> = function ({ mode }) {
   
           console.log(lat, lon);
   
-          return { lat, lon };
+          resolve({ lat, lon });
         },
-        () => ({ lat: 37.57002838826, lon: 126.97962084516 }),
+        () => resolve({ lat: 37.57002838826, lon: 126.97962084516 })
       );
-    }catch(e) {
-      console.log(e)
-    }
-    return { lat: 37.57002838826, lon: 126.97962084516 };
+    });
   }
 
   useEffect(() => {
     const container = document.getElementById('map');
-    const { lat, lon } = getCurrentPosition();
-    console.log(lat, lon);
-    const options = {
-      center: new window.kakao.maps.LatLng(lat, lon),
-      level: 4,
-    };
-    // eslint-disable-next-line no-new
-    map.current = new window.kakao.maps.Map(container, options);
+
+    (async () => {
+      const { lat, lon } = await getCurrentPosition();
+      console.log(lat, lon);
+      const options = {
+        center: new window.kakao.maps.LatLng(lat, lon),
+        level: 4,
+      };
+      // eslint-disable-next-line no-new
+      map.current = new window.kakao.maps.Map(container, options);
+  
+      const dbRef = ref(getDatabase());
+  
+      get(child(dbRef, `places/`))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+            const stores = Object.entries(snapshot.val()).map((item: any[]) => {
+              console.log(item);
+              return {
+                position: item[1].position,
+                placeName: item[1].placeName
+              }
+            })
+  
+            setList(stores);
+            stores.forEach(({position, placeName}) => {
+
+              console.log(position);
+
+              const imageSrc = markerImageSrc;
+              const imageSize = new window.kakao.maps.Size(50, 50);
+              const imageOption = { offset: new window.kakao.maps.Point(25, 46.5) };
+        
+              const markerImage = new window.kakao.maps.MarkerImage(
+                imageSrc,
+                imageSize,
+                imageOption
+              );
+              const markerPosition = new window.kakao.maps.LatLng(position.lat, position.lng);
+        
+              const marker = new window.kakao.maps.Marker({
+                map: map.current,
+                position: markerPosition,
+                image: markerImage,
+                title: placeName
+              });     
+            })
+        } else {
+          setList([]);
+        }
+      })
+      .catch((error) => {
+        // setError(error);
+      })
+      .finally(() => {
+        // setLoading(false);
+      });
+    })()
   }, [mode]);
 
   return (
