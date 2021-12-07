9a3998cf446c11ab5773276cbd373e64f3d73994
ryuhangyeong <hangyeong.ryu@gmail.com>
2021-12-07 20:37:09 +0900

    add: Place page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import React, { useState } from 'react';
-import Add from './pages/Add';
-import Home from './pages/Home';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import './assets/scss/styles.scss';
+import React, { useState } from "react";
+import Add from "./pages/Add";
+import Home from "./pages/Home";
+import Place from "./pages/Place";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./assets/scss/styles.scss";
 
 const App: React.FC = function () {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/add" element={<Add/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/add" element={<Add />} />
+        <Route path="/place/:uniqueId" element={<Place />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Place/index.tsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+const Place: React.FC = function () {
+  return <div>지도</div>;
+};
+
+export default Place;
